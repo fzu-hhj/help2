65c8e288c2248b9052af2d36aeaacbf30c1b003a
hhj <2571402106@qq.com>
2020-12-27 20:38:08 +0800

    添加了UserDAO进行个性化查询

---
+++ src/main/java/fzu/hhj/help2/dao/UserDAO.java
@@ -0,0 +1,38 @@
+package fzu.hhj.help2.dao;
+
+import fzu.hhj.help2.mapper.UserMapper;
+import fzu.hhj.help2.pojo.User;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class UserDAO {
+    @Autowired
+    UserMapper userMapper;
+
+    /**
+     * 查询数据库是否存在此用户名
+     * @param username 用户名
+     * @return 是否存在，true表示存在，false表示不存在
+     */
+    public boolean hasUserName(String username){
+        User user = new User();
+        user.setName(username);
+        if(userMapper.select(user) != null){
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 查询数据库中是否存在此邮箱
+     * @param email 邮箱
+     * @return 是否存在，true表示存在，false表示不存在
+     */
+    public boolean hasEmail(String email){
+        User user = new User();
+        user.setEmail(email);
+        if(userMapper.select(user) != null){
+            return true;
+        }
+        return false;
+    }
+}
