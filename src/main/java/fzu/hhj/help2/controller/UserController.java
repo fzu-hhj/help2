755c57e76ec9aec072928bb9d5ca56da8d72fde2
hhj <2571402106@qq.com>
2020-12-28 08:10:17 +0800

    删除不必要的import

--- src/main/java/fzu/hhj/help2/controller/UserController.java
+++ src/main/java/fzu/hhj/help2/controller/UserController.java
@@ -1,17 +1,11 @@
 package fzu.hhj.help2.controller;
 
-
-import fzu.hhj.help2.Util.ConstantUtil;
-import fzu.hhj.help2.Util.ServletUtil;
-
 import fzu.hhj.help2.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpSession;
-import java.util.HashMap;
 import java.util.Map;
 
 @RestController
